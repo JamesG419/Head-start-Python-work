ff63b89dd20b91ea728161999c3f37f06b870a8e
James Glenister <jglen419@gmail.com>
2018-11-08 19:25:47 -0500

    Update Chapter 2/panic.py
    Changed from inserting new characters and instead using letters from the list

--- Chapter 2/panic.py
+++ Chapter 2/panic.py
@@ -3,13 +3,12 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 
-for i in range(5):
+for i in range(4):
     plist.pop()
 plist.pop(0)
-plist.remove(' ')
 plist.remove("'")
-plist.insert(2,' ')
-plist.insert(4,'a')
+plist.extend([plist.pop(),plist.pop()])
+plist.insert(2,plist.pop(3))
 
 new_phrase = ''.join(plist)
 print(plist)
