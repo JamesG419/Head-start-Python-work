4dd2811e1112b0c1ecaf4153ebed4d07fe856a32
James Glenister <jglen419@gmail.com>
2018-11-13 10:22:58 -0500

    Update Chapter 5/hello_flask.py
    added search4letters capability to web app

--- Chapter 5/hello_flask.py
+++ Chapter 5/hello_flask.py
@@ -1,6 +1,7 @@
 
 
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
@@ -8,4 +9,7 @@ app = Flask(__name__)
 def hello() -> set:
 	return 'Hello world from Flask!'
 
+@app.route('/search4')
+def do_search() -> str:
+	return str(search4letters('life, the universe, and everything' , 'eiru!'))
 app.run()
