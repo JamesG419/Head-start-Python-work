12373bdf5008961f901b534d738e333d3c677ddd
James Glenister <jglen419@gmail.com>
2018-11-13 14:47:01 -0500

    Update Chapter 5/vsearch4web.py
    brings in requests from webpage and renders the results

--- Chapter 5/vsearch4web.py
+++ Chapter 5/vsearch4web.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -10,8 +10,16 @@ def hello() -> set:
 	return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-	return str(search4letters('life, the universe, and everything' , 'eiru!'))
+def do_search() -> 'html':
+	phrase = request.form['phrase']
+	letters = request.form['letters']
+	title = 'Here are your results: '
+	results = str(search4letters(phrase , letters))
+	return render_template('results.html',
+							the_phrase=phrase,
+							the_letters=letters,
+							the_title=title,
+							the_results=results,)
 
 @app.route('/entry')
 def entry_page() -> 'html':
