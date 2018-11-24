4ce27a1c2b62c1b144a0f1db04c34e54e2cee178
James Glenister <jglen419@gmail.com>
2018-11-24 12:55:05 -0500

    added a poage for the weblog

--- Chapter 5/vsearch4web.py
+++ Chapter 5/vsearch4web.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-	with open('vsearch.txt', 'a') as log:
+	with open('vsearch.log', 'a') as log:
 		print(req.form, req.remote_addr, req.user_agent, res, file = log, sep='|')
-		print(req.remote_addr, file = log)
-		print(req.user_agent, file = log)
-		print(res, file = log)
+		
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -33,10 +31,20 @@ def entry_page() -> 'html':
 	return render_template('entry.html' , the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
-	with open('vsearch.txt') as log:
-		contents = log.read()
-	return escape(contents)
+def view_the_log() -> 'html':
+	contents = []
+	with open('vsearch.log') as log:
+		for line in log:
+			contents.append([])
+			for item in line.split('|'):
+				contents[-1].append(escape(item))
+				
+	titles = ('Form Data', 'Remote_adr', 'User_agent', 'Results')
+	
+	return render_template('viewlog.html',
+							the_title='View Log',
+							the_row_titles=titles,
+							the_data=contents,)
 	
 if __name__=='__main__':
 	app.run(debug = True)
